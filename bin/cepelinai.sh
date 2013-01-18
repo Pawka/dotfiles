b9544a8483fbea807a37813ceb7e33e26bd4af89
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-01-18 11:17:41 +0200

    Fixed cepelinai for bash/sh

--- bin/cepelinai.sh
+++ bin/cepelinai.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-H="Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" && A=`curl --silent -A $H http://www.cepelinai.eu/lt/uzsakymas` && echo `echo $A | grep "product-name" | sed 's/<[^>]*>//g' | shuf -n 1` && echo `echo $A | grep \"order_sauce\" -A 5 | grep option | sed 's/<[^>]*>//g' | shuf -n 1`
+echo `curl --silent -A "Mozilla/5.0" http://www.cepelinai.eu/lt/uzsakymas | grep "product-name" | sed 's/<[^>]*>//g' | shuf -n 1` && echo `curl --silent -A "Mozilla/5.0" http://www.cepelinai.eu/lt/uzsakymas | grep \"order_sauce\" -A 5 | grep option | sed 's/<[^>]*>//g' | shuf -n 1`
